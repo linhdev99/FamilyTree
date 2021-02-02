643ac436e14536c775a3de4cbd6ef86c653e6a3b
Sasuke99 <linh.huynh.dev.99@gmail.com>
2021-02-02 18:31:44 +0700

    fix bug UI làm lại toàn bộ

--- sources/app/src/main/java/com/bkuhp2l/familytree2020/ui/login/LoginViewModel.java
+++ sources/app/src/main/java/com/bkuhp2l/familytree2020/ui/login/LoginViewModel.java
@@ -10,7 +10,7 @@ public class LoginViewModel extends ViewModel {
 
     public LoginViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("This is Login fragment");
     }
 
     public LiveData<String> getText() {
